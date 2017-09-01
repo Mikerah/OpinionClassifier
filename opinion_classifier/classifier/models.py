61d84c6404b9dd5370526207cd965c2d8d4a9ed7
Mikerah <mikerah14@gmail.com>
2017-08-31 20:03:51 -0400

    Completed django model for article, added a py file for data cleaning the mongoDB databases, included debugged items.py, pipelines.py and articles_spider.py

--- opinion_classifier/classifier/models.py
+++ opinion_classifier/classifier/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    """
+    Model for a New York Times article
+    """
+    url = models.CharField(max_length=250)
+    author = models.CharField(max_length = 200)
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    type = models.CharField(max_length=100)
