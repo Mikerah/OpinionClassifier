d15b2e35e828942872219e250163fcae44c8c84c
Mikerah <mikerah14@gmail.com>
2017-09-04 16:33:45 -0400

    Completed Data cleaning. Added skeleton code for index.html and views.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,8 @@
+"""
+This file contains all the code I ran in the python shell to clean the database
+"""
+
 import pymongo
-import nltk
 
 # Retrieve New York Times articles database and collection
 client = pymongo.MongoClient()
@@ -9,27 +12,28 @@ all_articles_collection = nyt_database['articles']
 # Check all the types of articles in the articles collection
 print(all_articles_collection.distinct('type'))
 
+# Find the number of opinion articles
+print(all_articles_collection.)
+
 # Combine types such as 'Sunday Review' into single type Opinion
 modified_collection_result = all_articles_collection.update_many(
-    {'type':'Sunday Review', 'type':'Book Review', 'type':'Public Editor'}, 
-    {'$set': {'type': 'Opinion'}})
+    {'type':'sunday-review'}, 
+    {'$set': {'type': 'opinion'}})
 
-# Combine repeat types into one type
-# movies and Movie
-modified_collection_result = all_articles_collection.update_many({'type':'movies'}, {'$set':{'type':'Movies'}})
-
-# Arts & Design and Arts
-modified_collection_result = all_articles_collection.update_many({'type':'Arts & Design'}, {'$set': {'type':'Arts'}})
-
-# Personal Tech and Technology
-modified_collection_result = all_articles_collection.update_many({'type': 'Personal Tech'}, {'$set': {'type':'Technology'}})
-
-# Combine types such as 'Soccer' and 'Baseball' into type Sports
 modified_collection_result = all_articles_collection.update_many(
-{'type': 'Soccer', 'type': 'Baseball', 'type':'Hockey', 'type': 'Golf', 'type':'Olympics', 'type':'College Football'
-'type': 'Pro Football', 'type':'Pro Basketball', 'type': 'College Basketball', 'type' : 'sports'},
-{'$set': {'type': 'Sports'}})
-
-# 
-
-# Count the number of opinion articles and news articles
+    {'type': 'public-editor'},
+    {'$set': {'type': 'opinion'}})
+    
+# Remove 'We’re interested in your feedback on this page'
+for i in all_articles_collection.find({}):
+    new_text = i['text'].replace("We’re interested in your feedback on this page.","")
+    all_articles_collection.update_one(i,{'$set':{'text': new_text}})
+    
+# Remove 'A version of this article ...'
+for i in all_articles_collection.find({}):
+    if 'A version of this article appears in print on' in i['text']:
+        beginning_index = i['text'].find('A version of this article appears in print on')
+        collection.update_one(i,{'$set':{'text':i['text'][:beginning_index]}})
+        
+# Remove Non-English articles from collection
+all_articles_collection.delete_many({'type':'universal'})
--- opinion_classifier/classifier/views.py
+++ opinion_classifier/classifier/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
+    """
+    Takes user to the main index page.
+    """
     render(request, 'classifier/index.html')
     
